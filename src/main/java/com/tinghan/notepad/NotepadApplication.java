6f0d11c526ea5a4e21196a322c96b3d4898fd42b
Tinghan_dy <tinghanlin@163.com>
2018-11-08 11:32:09 +0800

    success

--- src/main/java/com/tinghan/notepad/NotepadApplication.java
+++ src/main/java/com/tinghan/notepad/NotepadApplication.java
@@ -2,17 +2,11 @@ package com.tinghan.notepad;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class NotepadApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NotepadApplication.class, args);
-	}
-	
-	@GetMapping("/")
-	public String index() {
-		return "index";
 	}
 }
