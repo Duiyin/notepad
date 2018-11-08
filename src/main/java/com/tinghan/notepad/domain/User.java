7f12970b44eb7f443d9ac50726c55cdec735afa5
Tinghan <tinghanlin@163.com>
2018-11-08 22:04:40 +0800

    unfinish is personal html

---
+++ src/main/java/com/tinghan/notepad/domain/User.java
@@ -0,0 +1,207 @@
+package com.tinghan.notepad.domain;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import com.tinghan.notepad.util.ID;
+import com.tinghan.notepad.util.Time;
+
+@Entity
+public class User {
+
+	@Id
+	private String id;
+
+	private String account;
+
+	private String password;
+
+	private Timestamp createtime;
+
+	private String role;
+
+	private String rolename;
+
+	// 以下为个人信息↓
+	private String headimgs; // 头像
+
+	private String nickname; // 昵称
+
+	private String autograph; // 个性签名
+
+	private String sex; // 性别
+
+	private String birthday; // 破蛋日
+
+	private String email;
+
+	private String mobile;
+
+	private String education;
+
+	private String location; // 所在地
+
+	private String introduce; // 个人介绍
+
+	private String idcard; // 标识符-编号
+
+	public enum ROLE {
+		STUDENTS, TEACHER, ADMIN, FOREVER;
+	}
+
+	public User() {
+		this.id = ID.uuid();
+		this.idcard = ID.Intercept();
+		this.createtime = Time.timestamp();
+		this.role = "STUDENTS";
+		this.rolename = "普通用户";
+		this.headimgs = "/images/default_head.png";
+		this.nickname = idcard;
+		this.autograph = "此人较懒，暂无信息";
+		this.sex = "保密";
+		this.birthday = "1990-01-01";
+		this.email = "保密";
+		this.mobile = "保密";
+		this.education = "保密";
+		this.location = "保密";
+		this.introduce = "保密";
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getAccount() {
+		return account;
+	}
+
+	public void setAccount(String account) {
+		this.account = account;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Timestamp getCreatetime() {
+		return createtime;
+	}
+
+	public void setCreatetime(Timestamp createtime) {
+		this.createtime = createtime;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	public String getRolename() {
+		return rolename;
+	}
+
+	public void setRolename(String rolename) {
+		this.rolename = rolename;
+	}
+
+	public String getHeadimgs() {
+		return headimgs;
+	}
+
+	public void setHeadimgs(String headimgs) {
+		this.headimgs = headimgs;
+	}
+
+	public String getNickname() {
+		return nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+	public String getAutograph() {
+		return autograph;
+	}
+
+	public void setAutograph(String autograph) {
+		this.autograph = autograph;
+	}
+
+	public String getSex() {
+		return sex;
+	}
+
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+
+	public String getBirthday() {
+		return birthday;
+	}
+
+	public void setBirthday(String birthday) {
+		this.birthday = birthday;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getMobile() {
+		return mobile;
+	}
+
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+
+	public String getEducation() {
+		return education;
+	}
+
+	public void setEducation(String education) {
+		this.education = education;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public String getIntroduce() {
+		return introduce;
+	}
+
+	public void setIntroduce(String introduce) {
+		this.introduce = introduce;
+	}
+
+	public String getIdcard() {
+		return idcard;
+	}
+
+	public void setIdcard(String idcard) {
+		this.idcard = idcard;
+	}
+}
