5e98476e6e351c0f8a448702a6a99375368686ec
Tinghan <tinghanlin@163.com>
2018-12-20 21:48:27 +0800

    1

---
+++ src/main/java/com/tinghan/notepad/service/UserService.java
@@ -0,0 +1,11 @@
+package com.tinghan.notepad.service;
+
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Component;
+
+@Component
+@Transactional
+public class UserService {
+	
+}
