bc519dcb6cf10f708b1fea7634f3a70fddd5f52a
Tinghan <tinghanlin@163.com>
2019-01-21 21:41:35 +0800

    register is success

--- src/main/java/com/tinghan/notepad/service/UserService.java
+++ src/main/java/com/tinghan/notepad/service/UserService.java
@@ -9,18 +9,36 @@ import org.springframework.stereotype.Component;
 import com.tinghan.notepad.dao.UserDao;
 import com.tinghan.notepad.domain.User;
 import com.tinghan.notepad.domain.UserDto;
+import com.tinghan.notepad.util.PasswordUtil;
+import com.tinghan.notepad.util.Result;
+import com.tinghan.notepad.util.ValidTool;
 
 @Component
 @Transactional
 public class UserService {
-	
+
 	@Autowired
 	private UserDao userDao;
-	
-	public void register(UserDto userDto){
-		User user = new User();
-		User repeat = userDao.getOneById(userDto.getAccount());
-		BeanUtils.copyProperties(userDto, user, User.class);
-		userDao.save(user);
+
+	public User register(UserDto userDto) {
+		try {
+			User user = new User();
+			User repeat = userDao.getOneById(userDto.getAccount());
+			if (null != repeat) {
+				return (User) Result.failed();
+			} else {
+				if (ValidTool.isEmail(userDto.getEmail())) {
+					BeanUtils.copyProperties(userDto, user, User.class);
+					user.setPassword(PasswordUtil.createPassword(userDto.getPassword()));
+					userDao.save(user);
+				}
+				else{
+					return user;
+				}
+			}
+			return user;
+		} catch (Exception e) {
+			throw e;
+		}
 	}
 }
